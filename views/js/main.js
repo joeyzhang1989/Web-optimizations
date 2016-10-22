744623fef843e60ce427d016553f2f6f5e09f302
chenglong zhang <chenglongzhang931@gmail.com>
2016-10-22 01:26:45 -0400

    log windowSize

--- views/js/main.js
+++ views/js/main.js
@@ -439,7 +439,7 @@ var resizePizzas = function(size) {
 
     var newSize = sizeSwitcher(size);
     var dx = (newSize - oldSize) * windowWidth;
-    console.log(dx);
+    console.log(windowWidth);
     return dx;
   }
 
